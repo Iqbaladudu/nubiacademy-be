9529dc0b562d0f90ae13f79df425705490f79923
iqbaladudu <iqbal.adudu@gmail.com>
2025-02-22 15:22:40 +0200

    edit config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -158,5 +158,16 @@ export default buildConfig({
       },
     },
   ],
-  email: nodemailerAdapter(),
+  email: nodemailerAdapter({
+    defaultFromAddress: 'norepy@nubiacademy.id',
+    defaultFromName: 'Nubi Academy',
+    transportOptions: {
+      host: process.env.SMTP_HOST,
+      port: 587,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    },
+  }),
 })
